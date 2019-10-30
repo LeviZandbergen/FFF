c84f07f3971b48933e4999f524a86a33c211a3c2
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-30 11:14:14 +0100

    added category filter

--- Includes/articleContent.php
+++ Includes/articleContent.php
@@ -2,31 +2,51 @@
 <div class="content">
     <div class="itemContainer">
         <div class="categorie">
-            <select>
-                <?php
-                $categorie = "SELECT * FROM categorie;";
-                $stmt = $db->prepare($categorie);
-                $stmt->execute(array());
-                $type = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                ?>
-                <?php
-                foreach ($type as $key => $value) {
-                    ?>
-                    <option><?php echo $type[$key]["type"] ?></option>
-                <?php } ?>
+            <form method="POST">
 
-            </select>
+                <select name="categorie" onchange="this.form.submit()">
+                    <option>------------</option>
+                    <option value="allArticles">alle artikelen</option>
+                    <?php
+                    $categorie = "SELECT * FROM categorie;";
+                    $stmt = $db->prepare($categorie);
+                    $stmt->execute(array());
+                    $type = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    foreach ($type as $key => $value) {
+                        echo '<option value="' . $type[$key]["type"] . '">' . $type[$key]["type"] . '</option>';
+                    } ?>
+
+                </select>
+            </form>
         </div>
+
         <?php
         $articles = "SELECT * FROM artikel";
         $stmt = $db->prepare($articles);
         $stmt->execute(array());
         $product_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        if (isset($_POST['categorie']))
+            if ($_POST['categorie'] == 'Huur') {
+                $articles = "SELECT * FROM artikel WHERE artikel_idCategorie = 2";
+                $stmt = $db->prepare($articles);
+                $stmt->execute(array());
+                $product_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else if ($_POST['categorie'] == 'Koop') {
+                $articles = "SELECT * FROM artikel WHERE artikel_idCategorie = 1";
+                $stmt = $db->prepare($articles);
+                $stmt->execute(array());
+                $product_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                $articles = "SELECT * FROM artikel";
+                $stmt = $db->prepare($articles);
+                $stmt->execute(array());
+                $product_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
         if (!empty($product_array)) {
             foreach ($product_array as $key => $value) {
                 ?>
-                <div class="product-item">
+                <div class="product-item" onclick="">
                     <div class="product-image"><img style="max-height: 100%; max-width: 100%"
                                                     src="../FFF/Images/<?php echo $product_array[$key]["afbeelding"]; ?>">
                     </div>
